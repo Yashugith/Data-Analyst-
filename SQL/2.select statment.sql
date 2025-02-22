4578d85d5d02c7d2e0faa54105ad6047810b1901
yashaswini <yashuyashaswini87520@gmail.com>
2025-02-22 10:39:32 +0530

    Add files via upload

---
+++ SQL/2.select statment.sql
@@ -0,0 +1,15 @@
+SELECT * 
+FROM parks_and_recreation.employee_demographics;
+
+
+select last_name,
+last_name,
+birth_date,
+age,
+(age +10) * 10 +10
+from parks_and_recreation.employee_demographics;
+
+
+
+SELECT distinct gender
+FROM parks_and_recreation.employee_demographics;

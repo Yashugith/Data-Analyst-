4979db02a693e46cc1a4453a36a4461370fe004d
yashaswini <yashuyashaswini87520@gmail.com>
2025-02-24 23:37:51 +0530

    Add files via upload

---
+++ SQL/temporary.sql
@@ -0,0 +1,30 @@
+-- Temporary Tables
+
+create temporary table temp_table
+(first_name varchar(50),
+last_name varchar(50),
+favorate_movie varchar(100)
+);
+
+select *
+from temp_table;
+
+insert into temp_table
+values('alex','freberg','lord of the rings:the two towers');
+
+select *
+from temp_table;
+
+
+select *
+from employee_salary;
+
+create temporary table salary_over_50k
+select *
+from employee_salary
+where salary >= 50000;
+
+select *
+from salary_over_50k
+
+
